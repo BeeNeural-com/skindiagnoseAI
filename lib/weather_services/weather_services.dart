c3b640e4b8a935e9eed7e91f6f501f73aefefc86
musharrafhamraz <musharrafhamraz02@gmail.com>
2025-05-08 00:13:21 +0500

    weather service class modified

--- lib/weather_services/weather_services.dart
+++ lib/weather_services/weather_services.dart
@@ -2,36 +2,15 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class WeatherData {
-  final double temperature;
-  final double lowestTemp;
-  final double highestTemp;
-  final int humidity;
-  final double precipitation;
-  final int pressure;
-  final double windSpeed;
-  final String mainCondition;
+  final int uvIndex;
 
   WeatherData({
-    required this.temperature,
-    required this.lowestTemp,
-    required this.highestTemp,
-    required this.humidity,
-    required this.precipitation,
-    required this.pressure,
-    required this.windSpeed,
-    required this.mainCondition,
+    required this.uvIndex,
   });
 
   factory WeatherData.fromJson(Map<String, dynamic> json) {
     return WeatherData(
-      mainCondition: json['current']['weather'][0]['main'],
-      temperature: json['current']['temp'],
-      lowestTemp: json['daily'][0]['temp']['min'],
-      highestTemp: json['daily'][0]['temp']['max'],
-      humidity: json['current']['humidity'],
-      precipitation: json['daily'][0]['rain'] ?? 0.0,
-      pressure: json['current']['pressure'],
-      windSpeed: json['current']['wind_speed'],
+      uvIndex: json['current']['uvi'],
     );
   }
 }
@@ -57,6 +36,8 @@ class WeatherService {
     final response = await http.get(
       Uri.parse('$baseUrl?lat=$lat&lon=$lon&units=metric&appid=$apiKey'),
     );
+
+    print(response.body);
 
     if (response.statusCode == 200) {
       final json = jsonDecode(response.body);
