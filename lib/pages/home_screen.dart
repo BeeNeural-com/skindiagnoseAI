a1ea2b736d0ee3758a797554e6ad3fac8007ecc0
musharrafhamraz <musharrafhamraz02@gmail.com>
2025-05-08 00:14:00 +0500

    completly changed the concept of the screen with different classes and widgets

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,13 +1,9 @@
-import 'package:dtreatyflutter/auth/login_screen.dart';
-// import 'package:dtreatyflutter/data_storage/local_data_sync.dart';
-// import 'package:dtreatyflutter/pages/read_local_data.dart';
-// import 'package:dtreatyflutter/pages/read_local_data.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:dtreatyflutter/auth/login_screen.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '../components/weather.dart';
 import '../components/common_disease.dart';
 import '../components/naviagtion_button.dart';
-import '../components/facts_renderer.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -28,17 +24,12 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Column(
             children: [
               SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.27,
+                  height: MediaQuery.of(context).size.height * 0.33,
                   child: const WeatherScreen()),
-              const SizedBox(
-                height: 30.0,
-              ),
               const CommonDisease(),
               const SizedBox(
                 height: 20.0,
               ),
-              const AgricultureFacts(),
-              // const Spacer(),
               const SizedBox(
                 height: 10.0,
               ),
@@ -58,44 +49,44 @@ class _HomeScreenState extends State<HomeScreen> {
                   const SizedBox(
                     width: 10,
                   ),
-                  Expanded(
-                    flex: 1,
-                    child: NaviagtionButton(
-                      icon: Icons.logout_outlined,
-                      left: 1.0,
-                      buttonText: "",
-                      onPressed: () {
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: const Text('Do you want to Logout?'),
-                              actions: <Widget>[
-                                TextButton(
-                                  child: const Text('No'),
-                                  onPressed: () {
-                                    Navigator.of(context)
-                                        .pop(); // Close the dialog
-                                  },
-                                ),
-                                TextButton(
-                                  child: const Text('Yes'),
-                                  onPressed: () async {
-                                    await FirebaseAuth.instance.signOut();
-                                    Navigator.of(context).pushReplacement(
-                                      MaterialPageRoute(
-                                          builder: (context) =>
-                                              const LoginScreen()),
-                                    );
-                                  },
-                                ),
-                              ],
-                            );
-                          },
-                        );
-                      },
-                    ),
-                  ),
+                  // Expanded(
+                  //   flex: 1,
+                  //   child: NaviagtionButton(
+                  //     icon: Icons.logout_outlined,
+                  //     left: 1.0,
+                  //     buttonText: "",
+                  //     onPressed: () {
+                  //       showDialog(
+                  //         context: context,
+                  //         builder: (BuildContext context) {
+                  //           return AlertDialog(
+                  //             title: const Text('Do you want to Logout?'),
+                  //             actions: <Widget>[
+                  //               TextButton(
+                  //                 child: const Text('No'),
+                  //                 onPressed: () {
+                  //                   Navigator.of(context)
+                  //                       .pop(); // Close the dialog
+                  //                 },
+                  //               ),
+                  //               TextButton(
+                  //                 child: const Text('Yes'),
+                  //                 onPressed: () async {
+                  //                   await FirebaseAuth.instance.signOut();
+                  //                   Navigator.of(context).pushReplacement(
+                  //                     MaterialPageRoute(
+                  //                         builder: (context) =>
+                  //                             const LoginScreen()),
+                  //                   );
+                  //                 },
+                  //               ),
+                  //             ],
+                  //           );
+                  //         },
+                  //       );
+                  //     },
+                  //   ),
+                  // ),
                 ],
               ),
 
