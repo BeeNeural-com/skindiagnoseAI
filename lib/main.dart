cbdccc2d372dbe117989d4c09b3b7e02815c88d6
musharrafhamraz <musharrafhamraz02@gmail.com>
2025-05-10 18:32:02 +0500

    changes made to keep API keys confidential

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,13 @@ import 'pages/splash_screen.dart';
 import 'pages/home_screen.dart';
 import 'pages/camera_screen.dart';
 import 'pages/common_disease_details.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  await dotenv.load(fileName: ".env");
 
   runApp(const MyApp());
 }
